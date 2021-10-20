9f25b6221fb0128cd9cb47c6ff072e7349c01535
lucholal57 <lucholal57@gmail.com>
2021-10-20 00:14:36 -0300

    Taller Serializer ventas

--- Django/Back-End_Django/inclusoft/taller/serializers.py
+++ Django/Back-End_Django/inclusoft/taller/serializers.py
@@ -29,3 +29,35 @@ class Informe_CuatrimestralSerializer(serializers.ModelSerializer):
         model = Informe_Cuatrimestral
         fields = '__all__'
         
+class Materiales_TallerTallerSerializer(serializers.ModelSerializer):
+    """
+        Serializador de materiales con el taller en conjuntoi por eso figuran dos veces la palabra traller
+    """
+    taller = TallerSerializer(read_only=True)
+    class Meta:
+        model = Materiales_Taller
+        fields = '__all__'
+        depth = 2
+        
+class Materiales_TallerSerializer(serializers.ModelSerializer):
+    """
+        Serializer de material del taller
+    """
+    class Meta:
+        model = Materiales_Taller
+        fields = '__all__'
+        
+class Ventas_TallerTallerSerializer(serializers.ModelSerializer):
+    """
+        Serializador de ventas taller con taller por eso el nombre repetido
+    """
+    taller = TallerSerializer(read_only=True)
+    class Meta:
+        model = Ventas_Taller
+        fields = '__all__'
+        depth = 2
+        
+class Ventas_TallerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ventas_Taller
+        fields = '__all__'
