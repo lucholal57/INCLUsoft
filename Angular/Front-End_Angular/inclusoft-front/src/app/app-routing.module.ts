be137e9ee77dfb39028a3083591a54c90d1154fd
Lucena Luciano <lucholal57@gmail.com>
2021-11-18 22:59:54 -0300

    Ultimo commit Front End Funcionando OK

--- Angular/Front-End_Angular/inclusoft-front/src/app/app-routing.module.ts
+++ Angular/Front-End_Angular/inclusoft-front/src/app/app-routing.module.ts
@@ -24,16 +24,16 @@ const routes: Routes = [
   {path:'inicio', component: InicioComponent},
   //Rutas Alumnos
   {path:'alumno', component: AlumnoComponent},
-  {path:'datos_adicionales', component: DatosAdicionalesComponent},
-  {path:'asistencia', component: AsistenciaComponent},
-  {path:'antecedentes_medicos', component: AntecedenteMedicoComponent},
-  {path:'patologias', component: PatologiaComponent},
-  {path:'enfermeria', component: EnfermeriaComponent},
-  {path:'acta_compromiso', component: ActaCompromisoComponent},
+  {path:'alumno/datos_adicionales', component: DatosAdicionalesComponent},
+  {path:'alumno/asistencia', component: AsistenciaComponent},
+  {path:'alumno/antecedentes_medicos', component: AntecedenteMedicoComponent},
+  {path:'alumno/patologias', component: PatologiaComponent},
+  {path:'alumno/enfermeria', component: EnfermeriaComponent},
+  {path:'alumno/acta_compromiso', component: ActaCompromisoComponent},
   //Rutas Personal
   {path:'personal', component: PersonalComponent},
   {path:'personal/asistencia', component: AsistenciaPersonalComponent},
-  {path: 'personal/permiso_salida', component: PermisoSalidaComponent},
+  {path:'personal/permiso_salida', component: PermisoSalidaComponent},
   {path:'personal/evaluacion_laboral', component: EvaluacionLabolarComponent},
   {path:'personal/entrega_proyecto', component: EntregaProyectoComponent},
   //Rutas Talleres
